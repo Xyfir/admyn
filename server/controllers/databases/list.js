a8e6e79b4cd60ffc357552cc3cb31077ee7ed331
MrXyfir <mr@xyfir.com>
2017-09-18 15:54:27 -0700

    Add controller for `GET databases`

---
+++ server/controllers/databases/list.js
@@ -0,0 +1,26 @@
+const mysql = require('lib/mysql');
+
+/*
+  GET databases
+  RETURN
+    string[] // names of databases
+*/
+module.exports = async function(req, res) {
+
+  const db = new mysql;
+
+  try {
+    db.connect(
+      Object.assign({}, req.expressql.database, { database: '' })
+    );
+    const rows = await db.query('SHOW DATABASES');
+    db.close();
+
+    res.json(rows.map(r => r.Database));
+  }
+  catch (err) {
+    db.close();
+    res.status(400).json({ error: err });
+  }
+
+}
