f73b1ec2fc900e59ec8bb1b33d7dbd12b89efdab
MrXyfir <mr@xyfir.com>
2017-09-18 15:53:35 -0700

    Add server/index

---
+++ server/index.js
@@ -0,0 +1,48 @@
+require('app-module-path').addPath(__dirname);
+
+/**
+ * Creates Express router for the admin panel route.
+ * @param {object} [options] - Currently unused and can be ignored.
+ * @return {Express.Router}
+ */
+module.exports = function(options) {
+
+  const router = require('express').Router();
+
+  /* DATABASES */
+  router.get(
+    '/databases',
+    require('controllers/databases/list')
+  );
+
+  /* DATABASES > TABLES */
+  router.get(
+    '/databases/:db/tables',
+    require('controllers/databases/tables/list')
+  );
+  router.get(
+    '/databases/:db/tables/:t/structure',
+    require('controllers/databases/tables/structure')
+  );
+
+  /* DATABASES > TABLES > ROWS */
+  router.get(
+    '/databases/:db/tables/:t/rows',
+    require('controllers/databases/tables/rows/find')
+  );
+  router.put(
+    '/databases/:db/tables/:t/rows',
+    require('controllers/databases/tables/rows/edit')
+  );
+  router.post(
+    '/databases/:db/tables/:t/rows',
+    require('controllers/databases/tables/rows/insert')
+  );
+  router.delete(
+    '/databases/:db/tables/:t/rows',
+    require('controllers/databases/tables/rows/delete')
+  );
+
+  return router;
+
+}
