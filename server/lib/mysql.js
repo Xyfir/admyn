54552c9d498342a43a369858ffa6f779cbd2cda7
MrXyfir <mr@xyfir.com>
2017-09-18 15:54:02 -0700

    Add server/lib/mysql

---
+++ server/lib/mysql.js
@@ -0,0 +1,54 @@
+const mysql = require('mysql');
+
+/**
+ * @typedef {object} MySQLConnectOptions
+ * @prop {string} host
+ * @prop {string} user
+ * @prop {string} [password]
+ * @prop {string} [database]
+ */
+
+/**
+ * Wraps mysql package and uses promises in place of callbacks.
+ */
+class MySQL {
+
+  /**
+   * Create connection to database.
+   * @param {MySQLConnectOptions} options
+   */
+  connect(options) {
+    this.cn = mysql.createConnection({
+      host: options.host,
+      user: options.user,
+      password: options.password || '',
+      database: options.database || ''
+    });
+  }
+
+  /**
+   * Sends query to database.
+   * @param {string} [sql] - SQL query string
+   * @param {any[]|object} [vars] - Variables to insert at '?' in sql
+   * @async
+   * @return {object[]}
+   */
+  query(sql = '', vars = []) {
+    return new Promise((resolve, reject) =>
+      this.cn.query(sql, vars, (err, res) =>
+        err ? reject(err) : resolve(res)
+      )
+    );
+  }
+
+  /**
+   * Closes the database connection.
+   */
+  close() {
+    this.cn && this.cn.end();
+    this.cn = null;
+  }
+
+}
+
+module.exports = MySQL;
