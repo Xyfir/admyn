00cb474ddad75527f825701b9c19f32b1078dbb8
MrXyfir <mr@xyfir.com>
2017-09-18 15:55:38 -0700

    Add controller for `DELETE databases/:db/tables/:t/rows`

---
+++ server/controllers/databases/tables/rows/delete.js
@@ -0,0 +1,37 @@
+const mysql = require('lib/mysql');
+
+/*
+  DELETE databases/:db/tables/:t/rows
+  REQUIRED
+    where: { field: value }
+  RETURN
+    {
+      affectedRows: number, changedRows: number, fieldCount: number,
+      message: string, serverStatus: number, warningCount: number
+    }
+*/
+module.exports = async function(req, res) {
+
+  const db = new mysql;
+
+  try {
+    db.connect(
+      Object.assign({}, req.expressql.database, {
+        database: req.params.db
+      })
+    );
+    const result = await db.query(
+      `DELETE FROM \`${req.params.t}\` WHERE ` +
+      Object.keys(req.body.where).map(k => `\`${k}\` = ?`).join(' AND'),
+      Object.keys(req.body.where).map(k => req.body.where[k])
+    );
+    db.close();
+  
+    res.json(result);
+  }
+  catch (err) {
+    db.close();
+    res.status(400).json({ error: err });
+  }
+
+}
