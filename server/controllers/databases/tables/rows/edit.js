ffb53d8ceb3dfd8f8ba66cb6e2d901c7ad530b38
MrXyfir <mr@xyfir.com>
2017-09-18 15:55:55 -0700

    Add `PUT databases/:db/tables/:t/rows`

---
+++ server/controllers/databases/tables/rows/edit.js
@@ -0,0 +1,44 @@
+const mysql = require('lib/mysql');
+
+/*
+  PUT databases/:db/tables/:t/rows
+  REQUIRED
+    set: { field: value }
+    where: { field: value }
+  RETURN
+    {
+      affectedRows: number, changedRows: number, fieldCount: number,
+      message: string, serverStatus: number, warningCount: number
+    }
+*/
+module.exports = async function(req, res) {
+
+  const db = new mysql;
+
+  try {
+    db.connect(
+      Object.assign({}, req.expressql.database, {
+        database: req.params.db
+      })
+    );
+
+    const sql = `UPDATE \`${req.params.t}\` SET ${
+      Object.keys(req.body.set).map(k => `\`${k}\` = ?`).join(', ')
+    } WHERE ${
+      Object.keys(req.body.where).map(k => `\`${k}\` = ?`).join(' AND ')
+    }`,
+    vars = [].concat(
+      Object.keys(req.body.set).map(k => req.body.set[k]),
+      Object.keys(req.body.where).map(k => req.body.where[k])
+    ),
+    result = await db.query(sql, vars);
+    db.close();
+  
+    res.json(result);
+  }
+  catch (err) {
+    db.close();
+    res.status(400).json({ error: err });
+  }
+
+}
