5081eb4a80c819945fb7ab4c7a19052570a58ff1
MrXyfir <mr@xyfir.com>
2017-09-18 15:55:07 -0700

    Add controller for `GET databases/:db/tables/:t/structure`

---
+++ server/controllers/databases/tables/structure.js
@@ -0,0 +1,31 @@
+const mysql = require('lib/mysql');
+
+/*
+  GET databases/:db/tables/:t/structure
+  RETURN
+    [{
+      Default: null|string, Extra: string, Field: string, Key: string,
+      Null: string, Type: string
+    }]
+*/
+module.exports = async function(req, res) {
+
+  const db = new mysql;
+
+  try {
+    db.connect(
+      Object.assign({}, req.expressql.database, {
+        database: req.params.db
+      })
+    );
+    const rows = await db.query(`DESCRIBE \`${req.params.t}\``);
+    db.close();
+
+    res.json(rows);
+  }
+  catch (err) {
+    db.close();
+    res.status(400).json({ error: err });
+  }
+
+}
