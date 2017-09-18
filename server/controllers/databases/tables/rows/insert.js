0c8409ec61017df2955a7846b29de11d92c1aba3
MrXyfir <mr@xyfir.com>
2017-09-18 15:56:08 -0700

    Add `POST databases/:db/tables/:t/rows`

---
+++ server/controllers/databases/tables/rows/insert.js
@@ -0,0 +1,37 @@
+const mysql = require('lib/mysql');
+
+/*
+  POST databases/:db/tables/:t/rows
+  REQUIRED
+    data: { field: value }
+  RETURN
+    {
+      affectedRows: number, changedRows: number, fieldCount: number,
+      insertId: number, message: string, serverStatus: number,
+      warningCount: number
+    }
+*/
+module.exports = async function(req, res) {
+
+  const db = new mysql;
+
+  try {
+    db.connect(
+      Object.assign({}, req.expressql.database, {
+        database: req.params.db
+      })
+    );
+    const result = await db.query(
+      `INSERT INTO \`${req.params.t}\` SET ?`,
+      req.body.data
+    );
+    db.close();
+  
+    res.json(result);
+  }
+  catch (err) {
+    db.close();
+    res.status(400).json({ error: err });
+  }
+
+}
