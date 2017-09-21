0f7e813340c29999290303c8df707d49d919d096
MrXyfir <mr@xyfir.com>
2017-09-20 20:33:33 -0700

    Add RowForm

---
+++ client/components/databases/tables/rows/Form.jsx
@@ -0,0 +1,105 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+// react-md
+import DatePicker from 'react-md/lib/Pickers/DatePickerContainer';
+import TextField from 'react-md/lib/TextFields';
+import Button from 'react-md/lib/Buttons/Button';
+import Paper from 'react-md/lib/Papers';
+
+export default class RowForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  onSubmit() {
+    const data = {};
+
+    this.props.structure.forEach(col => {
+      if (/^date/.test(col.Type))
+        data[col.Field] = this.refs[col.Field].state.value;
+      else
+        data[col.Field] = this.refs[col.Field].value;
+    });
+
+    this.props.onSubmit(data);
+  }
+
+  render() {
+    const {structure, row = {}} = this.props;
+
+    return (
+      <Paper
+        zDepth={this.props.row ? 0 : 1}
+        component='section'
+        className='row-form'
+      >
+        {structure.map(col => {
+          if (/text/.test(col.Type)) return (
+            <TextField
+              id={'col-' + col.Field}
+              key={col.Field}
+              ref={col.Field}
+              rows={2}
+              type='text'
+              label={col.Field}
+              maxRows={10}
+              helpText={col.Type}
+              className='md-cell'
+              defaultValue={row[col.Field]}
+            />
+          )
+          if (/int\b/.test(col.Type)) return (
+            <TextField
+              id={'col-' + col.Field}
+              key={col.Field}
+              ref={col.Field}
+              type='number'
+              label={col.Field}
+              helpText={col.Type}
+              className='md-cell'
+              defaultValue={row[col.Field]}
+            />
+          )
+          if (/^date/.test(col.Type)) return (
+            <DatePicker
+              id={'col-' + col.Field}
+              key={col.Field}
+              ref={col.Field}
+              label={col.Field}
+              helpText={col.Type}
+              className='md-cell'
+              defaultValue={row[col.Field]}
+            />
+          )
+
+          return (
+            <TextField
+              id={'col-' + col.Field}
+              key={col.Field}
+              ref={col.Field}
+              type='text'
+              label={col.Field}
+              helpText={col.Type}
+              className='md-cell'
+              defaultValue={row[col.Field]}
+            />
+          )
+        })}
+
+        <Button
+          raised primary
+          onClick={() => this.onSubmit()}
+        >Submit</Button>
+      </Paper>
+    )
+  }
+
+}
+
+RowForm.propTypes = {
+  structure: PropTypes.arrayOf(PropTypes.object).isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  row: PropTypes.object
+};
