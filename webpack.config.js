183a87bfc1a4a43b8431fa451dbfb6d38408f57b
MrXyfir <mr@xyfir.com>
2017-09-30 20:23:36 -0700

    Build client/components/App.jsx to client/index.js

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  
+  entry: './client/components/App.jsx',
+  
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'client')
+  },
+
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.js', '.jsx']
+  },
+
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      include: [
+        path.resolve(__dirname, 'client/components')
+      ],
+      exclude: /node_modules/,
+      options: {
+        presets: ['env', 'react']
+      }
+    }]
+  },
+
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: false,
+      compress: { unused: false }
+    })
+  ]
+
+};
