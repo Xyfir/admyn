3f7f06ef280192a2679ee3104999b1c0c14aae92
MrXyfir <mr@xyfir.com>
2017-11-23 12:50:59 -0800

    Add database / table navigator in Drawer

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,7 @@
+import { Toolbar, ListItem, Drawer, Button } from 'react-md';
 import PropTypes from 'prop-types';
+import request from 'superagent';
 import React from 'react';
-
-// react-md
-import Toolbar from 'react-md/lib/Toolbars';
-import Button from 'react-md/lib/Buttons/Button';
 
 // Components
 import Databases from './databases/Databases';
@@ -13,10 +11,62 @@ export default class AdmynPanel extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { url: location.hash.substr(2).split('/') };
+    this.state = {
+      url: location.hash.substr(2).split('/'),
+      drawer: false,
+      databases: [/*{
+        name: string, tables: string[], expand: boolean
+      }*/]
+    };
 
-    window.onhashchange = () =>
-      this.setState({ url: location.hash.substr(2).split('/') });
+    window.onhashchange = () => {
+      const url = location.hash.substr(2).split('/');
+
+      if (
+        (this.state.url[1] && url[1] && this.state.url[1] != url[1]) ||
+        (this.state.url[3] && url[3] && this.state.url[3] != url[3])
+      )
+        location.reload();
+      else
+        this.setState({ url  });
+    }
+  }
+
+  onLoadDatabases() {
+    if (this.state.databases.length) return;
+
+    request
+      .get(this.props.api + 'databases')
+      .end((err, res) => !err &&
+        this.setState({
+          databases: res.body.map(db => Object({ name: db, tables: [] }))
+        })
+      );
+  }
+
+  /** @param {string} db */
+  onLoadTables(db) {
+    const databases = this.state.databases.slice();
+    const i = databases.findIndex(_db => _db.name == db);
+
+    if (databases[i].expand) {
+      // ** FIX: For some reason this causes an error, likely from react-md
+      // databases[i].expand = false;
+      return this.setState({ databases });
+    }
+
+    if (databases[i].tables.length) return;
+
+    request
+      .get(`${this.props.api}databases/${db}/tables`)
+      .end((err, res) => {
+        if (err) return;
+
+        databases[i].tables = res.body,
+        databases[i].expand = true;
+
+        this.setState({ databases });
+      });
   }
 
   render() {
@@ -39,6 +89,44 @@ export default class AdmynPanel extends React.Component {
             />
           ]}
           title={this.props.title}
+          nav={
+            <Button
+              icon
+              iconChildren='menu'
+              onClick={() =>
+                !this.setState({ drawer: true }) &&
+                this.onLoadDatabases()
+              }
+            />
+          }
+        />
+
+        <Drawer
+          onVisibilityChange={v => this.setState({ drawer: v })}
+          autoclose={true}
+          navItems={
+            this.state.databases.map(db =>
+              <ListItem
+                key={db.name}
+                visible={db.expand}
+                onClick={e => this.onLoadTables(db.name)}
+                primaryText={db.name}
+                nestedItems={
+                  db.tables.map(tbl =>
+                    <ListItem
+                      key={tbl}
+                      onClick={() => location.hash =
+                        `#/databases/${db.name}/tables/${tbl}/rows`
+                      }
+                      primaryText={tbl}
+                    />
+                  )
+                }
+              />
+            )
+          }
+          visible={this.state.drawer}
+          type={Drawer.DrawerTypes.TEMPORARY}
         />
 
         {view}
